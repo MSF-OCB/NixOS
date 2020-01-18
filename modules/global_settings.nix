c2c5d2516ae64b5c75acc66155f570e5b4111b06
R-VdP <r-vdp@users.noreply.github.com>
2020-01-18 01:07:20 +0100

    Refactor in preparation for the rescue ISO.

--- modules/global_settings.nix
+++ modules/global_settings.nix
@@ -20,7 +20,7 @@
   ];
 
   # python3 -c 'import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))'
-  users.extraUsers.msfocb.hashedPassword = lib.mkDefault
+  users.users.msfocb.hashedPassword = lib.mkDefault
     "$6$FLtTyAhwtSyaTU1T$tnDBA5S5YeUF/UdO2KZtCVNieUlKGlL4iyGYtVKPI3Vfbyiu1BIH2j.BcScXPZbBbZ1P9PwKKh5B7lNoPr9o31";
 
 }
