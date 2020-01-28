d405075adb3655db5828b112fb92049492ba272a
Mohammad Ullah (Munna) <munnabcis@gmail.com>
2020-01-28 14:49:32 +0100

    adding users

--- hosts/benuc037.nix
+++ hosts/benuc037.nix
@@ -18,8 +18,20 @@
       enable = true;
       remote_forward_port = 6037;
     };
+    
     crypto.enable = true;
     docker.enable = true;
+    
+    users.users = {
+      yusuph.enable = true;
+      damien.enable = true;
+      didier.enable = true;
+      joana.enable  = true;
+      kathy.enable  = true;
+      pasquale.enable  = true;
+      godfried.enable = true;
+      vini.enable = true;
+    };    
   };
 
 }
