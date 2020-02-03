b1990a58bd8c5de858781d64248cc435795b8a04
R-VdP <r-vdp@users.noreply.github.com>
2020-02-03 16:11:59 +0100

    Set the default nixos channel in global_settings.nix.

--- modules/global_settings.nix
+++ modules/global_settings.nix
@@ -10,10 +10,13 @@
 
 { lib, ... }:
 
+with lib;
+
 {
+  settings.system.nix_channel = mkDefault "19.09";
 
   # python3 -c 'import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))'
-  users.users.msfocb.hashedPassword = lib.mkDefault
+  users.users.msfocb.hashedPassword = mkDefault
     "$6$FLtTyAhwtSyaTU1T$tnDBA5S5YeUF/UdO2KZtCVNieUlKGlL4iyGYtVKPI3Vfbyiu1BIH2j.BcScXPZbBbZ1P9PwKKh5B7lNoPr9o31";
 
   settings.reverse_tunnel = {
