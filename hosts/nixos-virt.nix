6490973cb77c297c875fafec10207d11e6cc24ed
R-VdP <r-vdp@users.noreply.github.com>
2020-02-06 11:07:46 +0100

    Turn the virtualbox module into a real module.

--- hosts/nixos-virt.nix
+++ hosts/nixos-virt.nix
@@ -16,11 +16,9 @@
     boot.mode = "uefi";
     reverse_tunnel.enable = false;
     crypto.enable = true;
+    docker.enable = true;
+    virtualbox.enable = true;
   };
-
-  imports = [
-    ../modules/virtualbox-guest.nix
-  ];
 
 }
 
