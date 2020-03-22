e2524ca51d2ffa4cff75bf2d30663eaebdc9be00
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-03-22 21:01:31 +0000

    Make sure that python3 is available in the environment.

--- org-spec/org.nix
+++ org-spec/org.nix
@@ -20,7 +20,7 @@ with lib;
 
   settings.system.nix_channel = mkDefault "19.09";
 
-  # python3 -c 'import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))'
+  # nix-shell --packages python3 --command "python3 -c 'import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))'"
   users.users.msfocb.hashedPassword = mkDefault
     "$6$FLtTyAhwtSyaTU1T$tnDBA5S5YeUF/UdO2KZtCVNieUlKGlL4iyGYtVKPI3Vfbyiu1BIH2j.BcScXPZbBbZ1P9PwKKh5B7lNoPr9o31";
 }
