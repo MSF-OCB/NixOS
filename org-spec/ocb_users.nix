d0f911f3e5aa7577ab74073f16cb132bdd6ab6a5
RdN-MSF <ramses.de.norre@brussels.msf.org>
2020-04-02 22:16:08 +0200

    Field support team has tunnel access to all NUCs.

--- org-spec/ocb_users.nix
+++ org-spec/ocb_users.nix
@@ -14,6 +14,7 @@ with lib;
 {
   settings.users.users = let
 
+    # Admin users have shell access, belong to the wheel group, and are enabled by default
     admin = {
       enable      = true;
       sshAllowed  = true;
@@ -21,12 +22,16 @@ with lib;
       canTunnel   = true;
       extraGroups = [ "wheel" "docker" ];
     };
+    # Users who can tunnel only
+    # These are not enabled by default and should be enabled on a by-server basis
     tunnelOnly = {
       enable     = mkDefault false;
       sshAllowed = true;
       hasShell   = false;
       canTunnel  = true;
     };
+    # Users who are tunnel-only but can tunnel to all NixOS servers
+    tunnelOnlyAllServers = tunnelOnly // { enabled = true; };
 
   in {
 
@@ -36,32 +41,37 @@ with lib;
     xavier   = admin;
     yves     = admin;
 
+    # Field support team
+    #ali      = tunnelOnlyAllServers;
+    deepak   = tunnelOnlyAllServers;
+    #paul     = tunnelOnlyAllServers;
+
     # unifield user is a legacy user for Pakistan UF,
     # we cannot easily change the username anymore
-    unifield     = tunnelOnly;
-    uf_sl_baama  = tunnelOnly;
-    uf_ke_embu   = tunnelOnly;
-    uf_sl_kenema = tunnelOnly;
-    uf_cd_puc    = tunnelOnly;
-    uf_cd_masisi = tunnelOnly;
-    uf_mz_maputo = tunnelOnly;
-    uf_mz_beira  = tunnelOnly;
-    uf_cd_goma  = tunnelOnly;
-    uf_bd_coxcoord  = tunnelOnly;
-    uf_cd_kinshasacoord  = tunnelOnly;
+    unifield       = tunnelOnly;
+    uf_bd_coxcoord = tunnelOnly;
+    uf_cd_goma     = tunnelOnly;
+    uf_cd_kinshasacoord = tunnelOnly;
+    uf_cd_masisi   = tunnelOnly;
+    uf_cd_puc      = tunnelOnly;
+    uf_ke_embu     = tunnelOnly;
+    uf_mz_beira    = tunnelOnly;
+    uf_mz_maputo   = tunnelOnly;
+    uf_sl_baama    = tunnelOnly;
+    uf_sl_kenema   = tunnelOnly;
 
+    # Karachi data encoder, to be migrated to std system for field user keys
+    salima   = tunnelOnly;
 
-    nicolas  = tunnelOnly;
     damien   = tunnelOnly;
-    deepak   = tunnelOnly;
     didier   = tunnelOnly;
     dirk     = tunnelOnly;
     godfried = tunnelOnly;
     joana    = tunnelOnly;
     kathy    = tunnelOnly;
     marco    = tunnelOnly;
+    nicolas  = tunnelOnly;
     vini     = tunnelOnly;
-    salima   = tunnelOnly;
     yusuph   = tunnelOnly // {
       hasShell    = true;
       extraGroups = [ "docker" ];
@@ -75,12 +85,12 @@ with lib;
     msfocb = {
       isNormalUser = true;
       extraGroups  = [ "wheel" ];
-      openssh.authorizedKeys.keyFiles = [];
+      openssh.authorizedKeys.keyFiles = mkForce [];
     };
 
     # Lock the root user
     root = {
-      hashedPassword = "!";
+      hashedPassword = mkForce "!";
     };
   };
 }
