37df70a3f5455e5e44d7b89a8f23ea7c752ff60f
RdN-MSF <ramses.de.norre@brussels.msf.org>
2020-04-02 16:46:38 +0200

    Enable virtualbox support for the UF VMs.

--- org-spec/hosts/bevm003.nix
+++ org-spec/hosts/bevm003.nix
@@ -4,6 +4,7 @@
   settings = {
     network.host_name = "bevm003";
     boot.mode = "uefi";
+    virtualbox.enable = true;
     reverse_tunnel.enable = true;
     crypto.enable = false;
     docker.enable = false;
