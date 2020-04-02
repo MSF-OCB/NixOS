20e7635edeb63c2901a32b5e8286c004ec8d2b31
R-VdP <r-vdp@users.noreply.github.com>
2020-04-02 10:42:04 +0200

    Make bevm001.nix

---
+++ org-spec/hosts/bevm001.nix
@@ -0,0 +1,12 @@
+{
+  time.timeZone = "Africa/Lubumbashi";
+
+  settings = {
+    network.host_name = "bevm001";
+    boot.mode = "uefi";
+    reverse_tunnel.enable = true;
+    crypto.enable = true;
+    docker.enable = false;
+  };
+}
+
