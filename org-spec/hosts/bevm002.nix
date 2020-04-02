abbbe04e90404c8e7d6b24e57913293de48f8666
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-04-02 09:48:16 +0000

    Update bevm002.nix

--- org-spec/hosts/bevm002.nix
+++ org-spec/hosts/bevm002.nix
@@ -5,7 +5,7 @@
     network.host_name = "bevm002";
     boot.mode = "uefi";
     reverse_tunnel.enable = false;
-    crypto.enable = true;
+    crypto.enable = false;
     docker.enable = false;
   };
 }
