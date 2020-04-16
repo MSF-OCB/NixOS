46be37f84620156e3fbe467dbb46c1a2157d26b4
Deepak Sharma <62808386+codeshark-sudo@users.noreply.github.com>
2020-04-16 11:07:20 +0200

    Create bevm008.nix

---
+++ org-spec/hosts/bevm008.nix
@@ -0,0 +1,10 @@
+{
+  time.timeZone = "Africa/Conakry";
+
+  settings = {
+    network.host_name = "bevm008";
+    boot.mode = "uefi";
+    virtualbox.enable = true;
+    reverse_tunnel.enable = true;
+  };
+}
