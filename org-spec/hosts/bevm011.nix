d7035e0a82620e2ab5c7a50947ba0f3aaa454349
Ali Diakite <33649273+alibadra@users.noreply.github.com>
2020-04-21 15:30:32 +0200

    Create bevm011.nix

---
+++ org-spec/hosts/bevm011.nix
@@ -0,0 +1,10 @@
+{
+  time.timeZone = "Africa/ Kinshasa";
+
+  settings = {
+    network.host_name = "bevm011";
+    boot.mode = "uefi";
+    virtualbox.enable = true;
+    reverse_tunnel.enable = true;
+  };
+}
