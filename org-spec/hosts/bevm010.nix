f736f0f25ee9a79d5bc7209bc98040dadf7a655b
PaulGhantous <62932746+PaulGhantous@users.noreply.github.com>
2020-04-21 13:06:54 +0200

    Update bevm010.nix

--- org-spec/hosts/bevm010.nix
+++ org-spec/hosts/bevm010.nix
@@ -2,7 +2,7 @@
   time.timeZone = "Africa/Bangui";
 
   settings = {
-    network.host_name = "bevm009";
+    network.host_name = "bevm010";
     boot.mode = "uefi";
     virtualbox.enable = true;
     reverse_tunnel.enable = true;
