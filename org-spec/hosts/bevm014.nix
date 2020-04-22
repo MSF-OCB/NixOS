c0a00d8d97045a7812e7c93799c4f7f3d4417405
Ali Diakite <33649273+alibadra@users.noreply.github.com>
2020-04-22 07:46:31 +0200

    Create bevm014.nix

---
+++ org-spec/hosts/bevm014.nix
@@ -0,0 +1,10 @@
+{
+  time.timeZone = "America/Caracas";
+
+  settings = {
+    network.host_name = "bevm014";
+    boot.mode = "uefi";
+    virtualbox.enable = true;
+    reverse_tunnel.enable = true;
+  };
+}
