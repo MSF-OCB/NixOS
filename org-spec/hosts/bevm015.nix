cc9854ecd0cbd486ee2b2fa9e22e7137aeefa316
PaulGhantous <62932746+PaulGhantous@users.noreply.github.com>
2020-04-28 14:43:58 +0200

    Create bevm015.nix

---
+++ org-spec/hosts/bevm015.nix
@@ -0,0 +1,10 @@
+{
+  time.timeZone = "Africa/Bamenda";
+
+  settings = {
+    network.host_name = "bevm015";
+    boot.mode = "uefi";
+    virtualbox.enable = true;
+    reverse_tunnel.enable = true;
+  };
+}
