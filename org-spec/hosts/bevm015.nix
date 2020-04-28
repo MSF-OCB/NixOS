8bff60ed9bba97270fc88d13caaaf1f667c2c516
R-VdP <141248+R-VdP@users.noreply.github.com>
2020-04-28 19:41:07 +0200

    bevm015 needs a static network config because fortinet does not do DHCP.

--- org-spec/hosts/bevm015.nix
+++ org-spec/hosts/bevm015.nix
@@ -2,9 +2,17 @@
   time.timeZone = "Africa/Bamenda";
 
   settings = {
-    network.host_name = "bevm015";
     boot.mode = "uefi";
     virtualbox.enable = true;
     reverse_tunnel.enable = true;
+    network = {
+      host_name = "bevm015";
+      static_ifaces.enp0s3 = {
+        address = "10.210.210.31";
+        prefix_length = 24;
+        gateway = "10.210.210.254";
+      };
+    };
   };
 }
+
