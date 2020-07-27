235ae75deb1cbfe20ccfd4bf22369f1c07e57405
PaulGhantous <62932746+PaulGhantous@users.noreply.github.com>
2020-07-27 10:40:48 +0200

    Create bevm018.nix

---
+++ org-spec/hosts/bevm018.nix
@@ -0,0 +1,10 @@
+{
+  time.timeZone = "America/Caracas";
+
+  settings = {
+    network.host_name = "bevm018";
+    boot.mode = "uefi";
+    virtualbox.enable = true;
+    reverse_tunnel.enable = true;
+  };
+}
