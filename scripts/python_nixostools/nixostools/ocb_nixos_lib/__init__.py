20d58388ae761f4e6cd45d93b89827aecf44cfb8
R-VdP <R-VdP@users.noreply.github.com>
2021-07-02 18:24:19 +0200

    Adapt the python scripts to deal with multiple tunnels files.

---
+++ scripts/python_nixostools/nixostools/ocb_nixos_lib/__init__.py
@@ -0,0 +1,21 @@
+
+import json
+import os
+import os.path
+
+from collections import ChainMap
+from typing import Mapping
+
+
+def read_tunnel_config(tunnel_config_path: str) -> Mapping:
+  if os.path.isfile(tunnel_config_path):
+    with open(tunnel_config_path, 'r') as f:
+      return json.load(f) # type: ignore
+  elif os.path.isdir(tunnel_config_path):
+    return ChainMap(
+      *[ read_tunnel_config(f.path)
+         for f in os.scandir(tunnel_config_path)
+         if f.is_file and os.path.splitext(f.name)[1] == '.json' ])
+  else:
+    raise FileNotFoundError(f'The given tunnel config path ({tunnel_config_path}) does not exist!')
+
