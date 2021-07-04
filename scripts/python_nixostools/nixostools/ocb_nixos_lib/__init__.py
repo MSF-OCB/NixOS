0b5b840068f4a9a2b00d2fb03a62d706b916cde4
R-VdP <R-VdP@users.noreply.github.com>
2021-07-04 12:11:21 +0200

    is_file is a function and not an attribute.

--- scripts/python_nixostools/nixostools/ocb_nixos_lib/__init__.py
+++ scripts/python_nixostools/nixostools/ocb_nixos_lib/__init__.py
@@ -15,7 +15,7 @@ def read_tunnel_config(tunnel_config_path: str) -> Mapping:
     return ChainMap(
       *[ read_tunnel_config(f.path)
          for f in os.scandir(tunnel_config_path)
-         if f.is_file and os.path.splitext(f.name)[1] == '.json' ])
+         if f.is_file() and os.path.splitext(f.name)[1] == '.json' ])
   else:
     raise FileNotFoundError(f'The given tunnel config path ({tunnel_config_path}) does not exist!')
 
